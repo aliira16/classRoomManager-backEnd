4ee57891e6e1c4ca3dd41043180b265c992dc6c6
aliira16 <rouaneali27@gmail.com>
2026-04-03 16:36:07 +0100

    make subjects router

---
+++ src/routes/subjects.ts
@@ -0,0 +1,71 @@
+import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
+import express from "express";
+import { error } from "node:console";
+import { departments, subjects } from "../db/schema";
+import { db } from "../db";
+
+const router = express.Router();
+
+// get all sebjects with optional search filtering and pqgination
+router.get("/", async (req, res) => {
+  try {
+    const { search, department, page = 1, limit = 10 } = req.query;
+    const currentPage = Math.max(1, +page);
+    const limitePerPage = Math.max(1, +limit);
+
+    const offset = (currentPage - 1) * limitePerPage;
+
+    const filterConditions = [];
+
+    // if search is provided, filter subjects by name or code
+    if (search) {
+      filterConditions.push(
+        or(
+          ilike(subjects.name, `%${search}%`),
+          ilike(subjects.code, `%${search}%`),
+        ),
+      );
+    }
+
+    // if department is provided, filter subjects by department name
+    if (department) {
+      filterConditions.push(ilike(departments.name, `%${department}%`));
+    }
+
+    const whereClause =
+      filterConditions.length > 0 ? and(...filterConditions) : undefined;
+
+    const countResult = await db
+      .select({ count: sql`<number>count(*)</number>` })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause);
+
+    const totalCount = Number(countResult[0]?.count || 0);
+
+    const subjectList = await db
+      .select({
+        ...getTableColumns(subjects),
+        department: { ...getTableColumns(departments) },
+      })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause)
+      .orderBy(desc(subjects.createdAt))
+      .limit(limitePerPage)
+      .offset(offset);
+
+    res.status(200).json({
+      data: subjectList,
+      pagination: {
+        page: currentPage,
+        limit: limitePerPage,
+        total: totalCount,
+        totalPages: Math.ceil(totalCount / limitePerPage),
+      },
+    });
+  } catch (e) {
+    console.error(`Get /subjects error: ${e}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
